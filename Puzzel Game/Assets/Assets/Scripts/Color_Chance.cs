ff293dee592b3c4171d27995b95e1c57f4effd76
Dirk de Meij <dirkdemeij@gmail.com>
2018-02-14 16:15:20 +0100

    oefen color chance

---
+++ Puzzel Game/Assets/Assets/Scripts/Color_Chance.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color_Chance : MonoBehaviour {
+    public MeshRenderer renderer;
+    public Color newColor;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            renderer.material.color = newColor;
+
+        }
+	}
+}
