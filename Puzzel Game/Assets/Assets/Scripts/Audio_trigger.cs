11fc5502da0aaed4f6799df2d4b3635935b462ed
Dirk de Meij <dirkdemeij@gmail.com>
2018-02-22 01:32:47 +0100

    start coding

---
+++ Puzzel Game/Assets/Assets/Scripts/Audio_trigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_trigger : MonoBehaviour {
+    public AudioClip clip;
+    public AudioSource aSource;
+	// Use this for initialization
+	void Start () {
+		aSource.clip = clip
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            AudioSource.Play();
+        }
+        AudioSource.PlayClipAtPoint(clip, transform.position);
+        aSource.clip = clip[Random.Range(0, clip.length)];
+        aSource.pitch = Random.Range(0,5f)
+
+	}
+}
---
+++ Puzzel Game/Assets/Assets/Scripts/Trigger_Script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_Script : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hallo");
+        if (!aSource.isPLaying)
+            aSource.Play();
+	}
+}
